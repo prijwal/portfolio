0fe2dfd65bf587cf371c61e15fe61d816edee480
prijwal-finfactor <prijwalk@finfactor.in>
2025-10-22 16:51:04 +0530

    deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "development" ? "/" : "/prijwal/",
+  base: mode === "development" ? "/" : "/portfolio/",
 }));
